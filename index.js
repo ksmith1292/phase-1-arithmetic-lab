f86a9b07feec61369f2d09f18c6fef0947d34932
Kevin <kevin_smith1292@yahoo.com>
2023-09-05 10:32:44 -0500

    woof

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+
+
+function getRandomInteger() {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+  }
+  
+  let random = getRandomInteger();
+  let isGreaterThanZero = random > 0;
+  
+  let num3 = 14;
+  let num4 = 5;
+  
+  let mod = num3 % num4;
+
+  let max= Math.max(1, 2, 3, 4, 5,20);
